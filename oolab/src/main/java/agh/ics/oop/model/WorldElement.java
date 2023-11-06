4cb300c9edfa311421458927f5b448df02e709bd
Xavvee <hkabzinski@student.agh.edu.pl>
2023-11-06 19:04:03 +0100

    Initial commit for lab5. Grass and interfaces stuff.

---
+++ oolab/src/main/java/agh/ics/oop/model/WorldElement.java
@@ -0,0 +1,17 @@
+package agh.ics.oop.model;
+
+public interface WorldElement {
+
+    /**
+     * Return position of an object
+     *
+     * Returns the position of wanted animal or grass.
+     */
+    Vector2d getPosition();
+
+    /**
+     * Converts standard String representation into a wanted one.
+     */
+    String toString();
+
+}
